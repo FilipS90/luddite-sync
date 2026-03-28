6e23e3bc7fefb4d9b9ad748e2c3ffc59eeb92f23
filips90 <filip.stojiljkovic90@yahoo.com>
2026-03-28 13:45:07 +0100

    correcting the incorrect removal of file system directory when server unfollows the specific dir

--- server-sync/src/main/java/com/fstojilj/luddite/sync/server/service/RootDirService.java
+++ server-sync/src/main/java/com/fstojilj/luddite/sync/server/service/RootDirService.java
@@ -1,7 +1,6 @@
 package com.fstojilj.luddite.sync.server.service;
 
 import com.fstojilj.luddite.sync.common.model.RootDir;
-import com.fstojilj.luddite.sync.common.util.FileSystemUtils;
 import com.fstojilj.luddite.sync.server.repository.RootDirRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -49,7 +48,6 @@ public class RootDirService {
         dirWatcherService.stopWatching(absolutePath);
         fileMetadataService.deleteAllForRootDir(id);
         rootDirRepository.deleteRootDirById(id);
-        FileSystemUtils.deleteDirectoryRecursively(java.nio.file.Path.of(absolutePath));
         log.info("Removed root dir: {} (id={})", absolutePath, id);
         return true;
     }
