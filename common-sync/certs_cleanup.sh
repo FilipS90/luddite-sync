af17e392a07cfe91ac89c99d9c74837b16c94cfd
filips90 <filip.stojiljkovic90@yahoo.com>
2026-04-16 13:09:34 +0200

    keystore cleanup script fix

--- common-sync/certs_cleanup.sh
+++ common-sync/certs_cleanup.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "=== Certificate Cleanup Script ==="
-echo "Deleting server keystores..."
+echo "Deleting keystores..."
 rm -f ../server-sync/src/main/resources/server-keystore.p12
+rm -f ../server-sync/src/main/resources/client-keystore.p12
 rm -f ../server-sync/src/main/resources/truststore.p12
-
-echo "Deleting client keystores..."
+rm -f ../client-sync/src/main/resources/server-keystore.p12
 rm -f ../client-sync/src/main/resources/client-keystore.p12
 rm -f ../client-sync/src/main/resources/truststore.p12
 
